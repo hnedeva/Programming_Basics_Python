2848f10130146cbfe3a12568a62efe650c12eeca
Hristina Nedeva <x_k_n@abv.bg>
2025-02-17 18:40:33 +0200

    Add files via upload

---
+++ 01.First_Steps_in_Coding_Lab/8_Zooshop.py
@@ -0,0 +1,6 @@
+dog_food = int(input())
+cat_food = int(input())
+dog_food_per_can = 2.50
+cat_food_per_can = 4
+total_price = (dog_food * dog_food_per_can) + (cat_food * cat_food_per_can)
+print(f"{total_price} lv.")
---
+++ 01.First_Steps_in_Coding_Lab/9_Landscaping.py
@@ -0,0 +1,6 @@
+area = float(input())
+price_per_square_meter = 7.61
+total_price = area * price_per_square_meter
+discount = total_price * 0.18
+print(f"The final price is: {total_price - discount} lv.")
+print(f"The discount is: {discount} lv.")
